c7f253860f9d90ff912a9c576078da9e052bba8e
siva.subramanian <sivasubramanian86@hotmail.com>
2026-03-07 19:04:29 +0530

    fix(api): completely remove all vite env lookups and strict hardcode url

--- web-dashboard/src/config/axios.js
+++ web-dashboard/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// Configure axios to use the backend server
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://s4sofpxni6.execute-api.ap-south-2.amazonaws.com/prod'
+// Configure axios to softly bypass overriding environment blocks
+const API_BASE_URL = 'https://s4sofpxni6.execute-api.ap-south-2.amazonaws.com/prod'
 
 axios.defaults.baseURL = API_BASE_URL
 
